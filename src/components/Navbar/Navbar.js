fb88d346a87aa03efb496ae230e0f4944ef7a1c6
krystiano13 <crystianotv@gmail.com>
2023-10-09 11:00:26 +0200

    create functions in reducer that saves and loads data

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,10 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className="width-100 p-3 pt-5 pb-5 bg"></nav>
+        <nav className="width-100 p-3 pt-5 pb-5 bg">
+            <button>Load Project</button>
+            <button>Save Project</button>
+        </nav>
     )
 }
 
--- src/reducers/codeReducer.js
+++ src/reducers/codeReducer.js
@@ -4,6 +4,7 @@ const initialState = {
   html: "<h1>Welcome to my code editor !!!</h1>",
   css: "",
   js: "//use document.querySelector('.Page'); if you want to add something using javascript :)",
+  projects: [],
 };
 
 export const codeSlice = createSlice({
@@ -21,6 +22,34 @@ export const codeSlice = createSlice({
     js: (state, action) => {
       state.js = action.payload;
       return state;
+    },
+    loadProjects: (state, action) => {
+      if (window.localStorage.getItem("projects")) {
+        state.projects = localStorage.getItem("projects");
+      } else {
+        state.projects = [];
+      }
+    },
+    saveProject: (state, action) => {
+      let id;
+
+      if (state.projects.length === 0) {
+        id = 0;
+      } else {
+        id = state.projects.length;
+      }
+
+      const projectData = {
+        id: id,
+        name: action.payload,
+        html: state.html,
+        css: state.css,
+        js: state.js,
+      };
+
+      state.projects.push(projectData);
+
+      localStorage.setItem("projects", state.projects);
     },
   },
 });
